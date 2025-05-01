aa6c3b2fae2d9822be002e9460aacdb46fce2568
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-05-01 20:05:06 +0300

    lab 2 done

---
+++ src/main/java/by/bsuir/webtech/ListDemo.java
@@ -0,0 +1,27 @@
+package by.bsuir.webtech;
+
+public class ListDemo {
+    private int listItemValue;
+    private String ListItemText;
+
+    public int getListItemValue() {
+        return listItemValue;
+    }
+
+    public void setListItemValue(int listItemValue) {
+        this.listItemValue = listItemValue;
+    }
+
+    public String getListItemText() {
+        return ListItemText;
+    }
+
+    public void setListItemText(String listItemText) {
+        ListItemText = listItemText;
+    }
+
+    public ListDemo(int listItemValue, String listItemText) {
+        this.listItemValue = listItemValue;
+        ListItemText = listItemText;
+    }
+}
--- src/main/java/by/bsuir/webtech/controller/IndexGetController.java
+++ src/main/java/by/bsuir/webtech/controller/IndexGetController.java
@@ -1,14 +1,26 @@
 package by.bsuir.webtech.controller;
 
+import by.bsuir.webtech.ListDemo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class IndexGetController {
 
+    List<ListDemo> items = List.of(
+            new ListDemo(1,"one"),
+            new ListDemo(2,"two"),
+            new ListDemo(3,"three")
+    );
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAttribute("message","Лабораторная работа 2");
+        model.addAttribute("listItems", items);
         return "index";
     }
 }
